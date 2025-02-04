ffa1378131c93a77447302f101ecbd9828fc4ac7
Richie <richie_github@grange-lane.co.uk>
2025-02-04 11:10:41 +0000

    changes

---
+++ desktop/G2Tools_setup.sh
@@ -0,0 +1,7 @@
+chmod +x ~/github/G2Tools/desktop/*.sh  # Make all shell scripts executable
+chmod +x ~/github/G2Tools/desktop/*.desktop  # Make all .desktop files executable
+
+# copy over the desktop shortcuts
+cp ~/github/G2Tools/desktop/*.desktop ~/Desktop
+echo G2Tools icons copied to Desktop
+
--- desktop/copy_icons.sh
+++
@@ -1,3 +0,0 @@
-cp ~/github/G2Tools/desktop/*.desktop ~/Desktop
-echo G2Tools icons copied to Desktop
-
