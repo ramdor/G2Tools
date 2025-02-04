aeabf371158c2a2c2c750b5d2783fdc85054638e
Richie <richie_github@grange-lane.co.uk>
2025-02-04 18:24:03 +0000

    Update g2_helper.py

--- g2_helper.py
+++ g2_helper.py
@@ -6,7 +6,7 @@ import os
 
 root = tk.Tk()
 root.title("G2 Helper - MW0LGE")
-root.geometry("680x512")  # Increased height to fit new button
+root.geometry("680x580")  # Increased height to fit new button
 root.resizable(False, False)
 
 # Warning Label
