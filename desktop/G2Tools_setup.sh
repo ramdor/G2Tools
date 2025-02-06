380d0e68862dbdc2b5ad5906e5dfdfb86108acb8
Richie <richie_github@grange-lane.co.uk>
2025-02-06 09:50:57 +0000

    changes

--- desktop/G2Tools_setup.sh
+++ desktop/G2Tools_setup.sh
@@ -2,7 +2,6 @@ chmod +x ~/github/G2Tools/desktop/*.sh  # Make all shell scripts executable
 chmod +x ~/github/G2Tools/desktop/*.desktop  # Make all .desktop files executable
 chmod +x ~/github/G2Tools/p2app_service.sh
 chmod +x ~/github/G2Tools/p2app_service_undo.sh
-chmod +x ~/github/G2Tools/G2_overclock.sh
 
 # copy over the desktop shortcuts
 cp ~/github/G2Tools/desktop/*.desktop ~/Desktop
